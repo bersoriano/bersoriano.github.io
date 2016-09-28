311f03825bb1342882766ae694c44471c46ba68e
Bersoriano <bersoriano@me.com>
2016-09-28 00:15:27 -0400

    updated controller

--- fintechberyrub/js/main.js
+++ fintechberyrub/js/main.js
@@ -3,59 +3,37 @@ console.log('Main JS file loaded');
 angular
 	.module('starterApp',['ngMaterial', 'ngMessages'])
 	.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('blue')
-    .accentPalette('orange');
+		$mdThemingProvider.theme('default')
+		    .primaryPalette('blue')
+		    .accentPalette('orange');
 })
 	.controller('BasicDemoCtrl', ['$scope', function($scope){
-
 		console.log('Controller launched');
-	    var self = this;
-	    self.readonly = false;
-	    // Lists of fruit names and Vegetable objects
-	    self.fruitNames = ['Apple', 'Banana', 'Orange'];
-	    self.roFruitNames = angular.copy(self.fruitNames);
-	    self.editableFruitNames = angular.copy(self.fruitNames);
-	    self.tags = [];
-	    self.vegObjs = [
-	      {
-	        'name' : 'Broccoli',
-	        'type' : 'Brassica'
-	      },
-	      {
-	        'name' : 'Cabbage',
-	        'type' : 'Brassica'
-	      },
-	      {
-	        'name' : 'Carrot',
-	        'type' : 'Umbelliferous'
-	      }
-	    ];
-	    self.newVeg = function(chip) {
-	      return {
-	        name: chip,
-	        type: 'unknown'
-	      };
-	    };
+		var vm = this;
 
-	    $scope.amountInit = '';
-        
-        self.creaString = function () {
-            var i;
-            var unArray = [];
-            for (i=0;i<500;i++) {
-                var contador = i;
-                unArray.push(contador);
-            }
-            console.log(unArray);
-            return unArray;
+	    vm.amountInit = '';
+	    vm.showScreen1 = true;
+	    vm.showScreen2 = false;
+	    vm.showScreen3 = false;
+	    vm.showScreen4 = false;
+
+        vm.confirm = function () {
+        	vm.showScreen1 = false;
+        	vm.showScreen2 =  true;
         }
 
-        self.whatever = self.creaString();
-
-        self.hide = function () {
-
+        vm.searchButton = function () {
+        	vm.showScreen2 =  false;
+        	vm.showScreen3 = true;
         }
+
+        vm.confirmUser = function () {
+        	vm.showScreen3 = false;
+        	vm.showScreen4 = true;
+        }
+
+
+
 
 
 	}]);
